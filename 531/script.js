e69c0ac801328ceb9193ef4b474f0b3cfeac58aa
realJoeMay <80790105+realJoeMay@users.noreply.github.com>
2024-03-17 22:35:24 -0400

    Copy shortcut url

--- 531/script.js
+++ 531/script.js
@@ -1,6 +1,6 @@
 function updateLift(lift) {
-	maxInput = maxLiftInput(lift);
-	max = Number(maxInput.value);
+	// maxInput = maxLiftInput(lift);
+	max = maxLiftNumber(lift);
 	liftClass = ".lift-" + lift;
 
 	if (isNaN(max)) {
@@ -33,6 +33,11 @@ function updateLift(lift) {
 	}
 }
 
+function maxLiftNumber(lift) {
+	maxInput = maxLiftInput(lift);
+	return Number(maxInput.value);
+}
+
 function maxLiftInput(lift) {
 	return document.getElementById("max-" + lift);
 }
@@ -46,21 +51,16 @@ function updatePlan() {
 
 function prefillInputs() {
 	const params = new URLSearchParams(window.location.search);
-
 	if (params.has('maxSquat')) {
 		maxLiftInput('squat').value = params.get('maxSquat');
 	}
-	
 	if (params.has('maxDeadlift')) {
 		maxLiftInput('deadlift').value = params.get('maxDeadlift');
 	}
-
 	if (params.has('maxBench')) {
 		maxLiftInput('bench').value = params.get('maxBench');
 	}
-
 	if (params.has('maxPress')) {
 		maxLiftInput('press').value = params.get('maxPress');
 	}
-
 }
